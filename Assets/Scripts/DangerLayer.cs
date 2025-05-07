51ea535fa1bb437714564c6e9d2615e4ac204eec
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2025-05-07 15:10:05 +0300

    Danger Layer changes

--- Assets/Scripts/DangerLayer.cs
+++ Assets/Scripts/DangerLayer.cs
@@ -1,4 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+public class InitializeDangerTokens
+{
+    public GameObject[] dangerTokens;
+    public int highestTokenIndexToActivate;
+
+    public InitializeDangerTokens(GameObject[] _dangerTokens, int _highestTokenIndexToActivate)
+    {
+        dangerTokens = _dangerTokens;
+        highestTokenIndexToActivate = _highestTokenIndexToActivate;
+    }
+}
 
 public class DangerLayer : MonoBehaviour
 {
@@ -9,13 +22,25 @@ public class DangerLayer : MonoBehaviour
     private void OnEnable()
     {
         EventBus.Subscribe<NewRoundStartedEvent>(NewRoundStarted);
+        EventBus.Subscribe<InitializeDangerTokens>(InitializeDangerTokens);
     }
 
     private void OnDisable()
     {
         EventBus.Unsubscribe<NewRoundStartedEvent>(NewRoundStarted);
+        EventBus.Unsubscribe<InitializeDangerTokens>(InitializeDangerTokens);
     }
 
+    public void InitializeDangerTokens(InitializeDangerTokens e)
+    {
+        dangerTokens = e.dangerTokens;
+        for (int i = 0; i < dangerTokens.Length; i++)
+        {
+            dangerTokens[i].SetActive(i <= e.highestTokenIndexToActivate);
+        }
+        nextTokenToActivate = e.highestTokenIndexToActivate + 1;
+    }
+    
     public void NewRoundStarted(NewRoundStartedEvent e)
     {
         if (nextTokenToActivate < dangerTokens.Length)
