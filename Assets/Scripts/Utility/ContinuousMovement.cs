ddf6afafd8f2d4e6d1dd67e947b15bd108d58f42
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2025-06-02 16:19:39 +0300

    Level Disappearing Pins - 3

--- Assets/Scripts/Utility/ContinuousMovement.cs
+++ Assets/Scripts/Utility/ContinuousMovement.cs
@@ -6,38 +6,48 @@ public class ContinuousMovement : MonoBehaviour
     public Transform pointATransform;
     public Transform pointBTransform;
 
-    [HideInInspector] public Vector3 pointA;
-    [HideInInspector] public Vector3 pointB;
+    [HideInInspector]
+    public Vector3 pointA;
+    [HideInInspector]
+    public Vector3 pointB;
+    
     public float speed = 1f;
 
-    private float t = 0f;
+    private float lerpFactor = 0f;
     private bool goingForward = true;
 
     private void Awake()
     {
-        if (pointATransform) pointA = pointATransform.position;
-        if (pointBTransform) pointB = pointBTransform.position;
+        if (pointATransform)
+            pointA = pointATransform.position;
+        if (pointBTransform)
+            pointB = pointBTransform.position;
+    }
+
+    private void OnEnable()
+    {
+        lerpFactor = 0f;
     }
 
     void Update()
     {
-        transform.position = Vector3.Lerp(pointA, pointB, t);
+        transform.position = Vector3.Lerp(pointA, pointB, lerpFactor);
 
         if (goingForward)
         {
-            t += Time.deltaTime * speed;
-            if (t >= 1f)
+            lerpFactor += Time.deltaTime * speed;
+            if (lerpFactor >= 1f)
             {
-                t = 1f;
+                lerpFactor = 1f;
                 goingForward = false;
             }
         }
         else
         {
-            t -= Time.deltaTime * speed;
-            if (t <= 0f)
+            lerpFactor -= Time.deltaTime * speed;
+            if (lerpFactor <= 0f)
             {
-                t = 0f;
+                lerpFactor = 0f;
                 goingForward = true;
             }
         }
