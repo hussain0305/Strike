56048fabe0983db7cc9484cc2fd391e0f7e195f5
Hussain Shauzab Abis <hussain0305@users.noreply.github.com>
2025-06-26 16:30:04 +0300

    HUD Tutorial fixes

--- Assets/Scripts/HUDTutorials.cs
+++ Assets/Scripts/HUDTutorials.cs
@@ -179,6 +179,7 @@ public class HUDTutorials : MonoBehaviour
     {
         if (!swipeTutorialShown)
         {
+            swipeTutorialShown = true;
             swipeTutorial.gameObject.SetActive(true);
         }
 
